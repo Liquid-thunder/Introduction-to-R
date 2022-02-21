f359bb6e7f3275932fe10a0cf3bbde9d0518dcea
Matthew Davis <97855506+Liquid-thunder@users.noreply.github.com>
2022-02-21 07:58:04 -0500

    Add files via upload

---
+++ Module 6.R
@@ -0,0 +1,14 @@
+
+
+A <- matrix(c(2,0,1,3), ncol=2)
+B <- matrix(c(5,2,4,-1), ncol=2)
+
+A+B
+A-B
+
+C <- diag(c(4,1,2,3), ncol = 4)
+C
+
+D <- matrix(c(1, 1, 1, 1, 1), nrow = 1)
+E <- matrix(c(1, 1, 1, 1, 1), ncol = 1)
+f <- matrix(c(3,1,1,1,1;2,3;2,0,3;2,0,0,3;2,0,0,0,3), ncol = 5, nrow = 5)
